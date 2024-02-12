f469300ddb2920735c521ea5b21b8a164fa01986
Mervin Diaz <mervindiazlugo@gmail.com>
2024-02-11 22:03:04 -0300

    Android Test

--- src/test/java/udemy/web/Page/PageBase.java
+++ src/test/java/udemy/web/Page/PageBase.java
@@ -1,14 +1,6 @@
 package udemy.web.Page;
 
-import java.util.List;
-
 import config.WebDriverHelper;
-import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.log4testng.Logger;
 
 public class PageBase extends WebDriverHelper {
@@ -16,5 +8,4 @@ public class PageBase extends WebDriverHelper {
   private static Logger log = Logger.getLogger(PageBase.class);
 
   private static final int EXPLICIT_TIMEOUT = 20;
-
 }
