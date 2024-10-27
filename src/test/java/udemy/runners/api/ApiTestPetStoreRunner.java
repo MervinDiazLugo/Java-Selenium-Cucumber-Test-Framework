82428d309000995ea98fe35944786e24308b7139
Mervin Diaz <mervindiazlugo@gmail.com>
2024-10-27 10:16:34 -0700

    Ongoing Fixes

---
+++ src/test/java/udemy/runners/api/ApiTestPetStoreRunner.java
@@ -0,0 +1,32 @@
+package udemy.runners.api;
+
+import config.RestAssuredConfigProperties;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import lombok.extern.java.Log;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+@Log
+@CucumberOptions(
+    tags = "@ApiTesting and not @Ignore",
+    features = "src/test/resources/udemy/ApiTest/PetStore",
+    glue = "udemy.StepDefinitions",
+    plugin = {
+      "pretty",
+      "summary",
+      "html:test-output",
+      "json:target/cucumber/cucumber.json",
+      "html:target/cucumber-html-report.html"
+    })
+public class ApiTestPetStoreRunner extends AbstractTestNGCucumberTests {
+  @BeforeTest
+  @Parameters({"webdriver.env", "webdriver.client"})
+  public void beforeSuite(@Optional("null") String environment, @Optional("null") String client) {
+    log.info("TestNG webdriver.env for this test set is " + environment);
+    log.info("TestNG webdriver.client for this test set is " + client);
+    RestAssuredConfigProperties.setTestNgEnvironment(environment);
+    RestAssuredConfigProperties.setTestNgClient(client);
+  }
+}
